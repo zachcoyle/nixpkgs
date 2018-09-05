9abc1ccffb81a6c4ab7d52a9f8d3412f10f42429
Jan Tojnar <jtojnar@gmail.com>
2018-09-05 00:42:47 +0000

    gnome3.bijiben: 3.28.3 → 3.30.0

--- pkgs/desktops/gnome-3/apps/bijiben/default.nix
+++ pkgs/desktops/gnome-3/apps/bijiben/default.nix
@@ -5,13 +5,13 @@
 , gnome3, libxml2 }:
 
 let
-  version = "3.28.3";
+  version = "3.30.0";
 in stdenv.mkDerivation rec {
   name = "bijiben-${version}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/bijiben/${gnome3.versionBranch version}/${name}.tar.xz";
-    sha256 = "0lg92fl6dmrybkxs3gqhyr8rq945y64k51l6s72yiads7pqabli2";
+    sha256 = "1xcbbhg7ldpl9mrdl5m6gk9pfakbmmam008n212d6317y3i2r78l";
   };
 
   doCheck = true;
