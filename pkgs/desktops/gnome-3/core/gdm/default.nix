5eaacaef3e0c9470e54b7b5eb66cf83c5bad3bc7
Jan Tojnar <jtojnar@gmail.com>
2018-09-05 00:44:24 +0000

    gnome3.gdm: 3.28.3 → 3.30.0

--- pkgs/desktops/gnome-3/core/gdm/default.nix
+++ pkgs/desktops/gnome-3/core/gdm/default.nix
@@ -1,15 +1,15 @@
 { stdenv, fetchurl, substituteAll, pkgconfig, glib, itstool, libxml2, xorg
-, intltool, accountsservice, libX11, gnome3, systemd, autoreconfHook
+, accountsservice, libX11, gnome3, systemd, autoreconfHook
 , gtk, libcanberra-gtk3, pam, libtool, gobjectIntrospection, plymouth
-, librsvg, coreutils, xwayland }:
+, librsvg, coreutils, xwayland, fetchpatch }:
 
 stdenv.mkDerivation rec {
   name = "gdm-${version}";
-  version = "3.28.3";
+  version = "3.30.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gdm/${gnome3.versionBranch version}/${name}.tar.xz";
-    sha256 = "12d1cp2dyca8rwh9y9cg8xn6grdp8nmxkkqwg4xpkr8i8ml65n88";
+    sha256 = "0nyamr10y668nrkmfplf261lbjb3ivx1xyh14chw5mn3cpqgmn44";
   };
 
   # Only needed to make it build
@@ -24,9 +24,10 @@ stdenv.mkDerivation rec {
     "--enable-gdm-xsession"
     "--with-initial-vt=7"
     "--with-systemdsystemunitdir=$(out)/etc/systemd/system"
+    "--with-udevdir=$(out)/lib/udev"
   ];
 
-  nativeBuildInputs = [ pkgconfig libxml2 itstool intltool autoreconfHook libtool gnome3.dconf ];
+  nativeBuildInputs = [ pkgconfig libxml2 itstool autoreconfHook libtool gnome3.dconf ];
   buildInputs = [
     glib accountsservice systemd
     gobjectIntrospection libX11 gtk
@@ -37,6 +38,11 @@ stdenv.mkDerivation rec {
 
   # Disable Access Control because our X does not support FamilyServerInterpreted yet
   patches = [
+    # configurable udev dir
+    (fetchpatch {
+      url = https://gitlab.gnome.org/GNOME/gdm/commit/ef231c2790e7a3bbee3f09ac4a125e28e95011b4.patch;
+      sha256 = "0nn409l1pb502ncza3bic4krabxvx6kp2wl2cjpcmnpqrpyjkq8v";
+    })
     (substituteAll {
       src = ./fix-paths.patch;
       inherit coreutils plymouth xwayland;
