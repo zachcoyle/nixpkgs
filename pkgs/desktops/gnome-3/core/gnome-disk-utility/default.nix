3f0949909df1d4521f36121353d2e22605860629
Jan Tojnar <jtojnar@gmail.com>
2018-09-05 00:46:29 +0000

    gnome3.gnome-disk-utility: 3.28.3 → 3.30.0

--- pkgs/desktops/gnome-3/core/gnome-disk-utility/default.nix
+++ pkgs/desktops/gnome-3/core/gnome-disk-utility/default.nix
@@ -5,11 +5,11 @@
 
 stdenv.mkDerivation rec {
   name = "gnome-disk-utility-${version}";
-  version = "3.28.3";
+  version = "3.30.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-disk-utility/${gnome3.versionBranch version}/${name}.tar.xz";
-    sha256 = "11ajz4cbsdns81kihd6242b6pwxbw8bkr9qqkf4qnb4kp363a38m";
+    sha256 = "1hwiha1z1mgh0cl9bmxc5jvg3acw4n10hn4wk8halacm0qzvjzwn";
   };
 
   passthru = {
