c157e35fa65d66dd8c1c3d618665bdede9853e7a
Jan Tojnar <jtojnar@gmail.com>
2018-09-05 00:46:22 +0000

    gnome3.gnome-desktop: 3.28.2 → 3.30.0

--- pkgs/desktops/gnome-3/core/gnome-desktop/default.nix
+++ pkgs/desktops/gnome-3/core/gnome-desktop/default.nix
@@ -1,25 +1,22 @@
 { stdenv, fetchurl, substituteAll, pkgconfig, libxslt, which, libX11, gnome3, gtk3, glib
-, intltool, libxml2, xkeyboard_config, isocodes, itstool, wayland
+, gettext, libxml2, xkeyboard_config, isocodes, itstool, wayland
 , libseccomp, bubblewrap, gobjectIntrospection, gtk-doc, docbook_xsl }:
 
 stdenv.mkDerivation rec {
   name = "gnome-desktop-${version}";
-  version = "3.28.2";
+  version = "3.30.0";
 
   outputs = [ "out" "dev" "devdoc" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-desktop/${gnome3.versionBranch version}/${name}.tar.xz";
-    sha256 = "0c439hhpfd9axmv4af6fzhibksh69pnn2nnbghbbqqbwy6zqfl30";
+    sha256 = "1zcmyxxf61hrmba2g5d7zhck4pfc5x2gf90hznh2b4cdy5m4yja5";
   };
-
-  # TODO: remove with 3.30
-  NIX_CFLAGS_COMPILE = "-I${glib.dev}/include/gio-unix-2.0";
 
   enableParallelBuilding = true;
 
   nativeBuildInputs = [
-    pkgconfig which itstool intltool libxslt libxml2 gobjectIntrospection
+    pkgconfig which itstool gettext libxslt libxml2 gobjectIntrospection
     gtk-doc docbook_xsl
   ];
   buildInputs = [
