e31589a29a937910daca4c0d38d94516e043c183
Tor Hedin Brønner <torhedinbronner@gmail.com>
2018-09-11 22:49:43 +0200

    pythonPackages.pygobject3: 3.26.1 -> 3.30.0

--- pkgs/development/python-modules/pygobject/3.nix
+++ pkgs/development/python-modules/pygobject/3.nix
@@ -1,8 +1,9 @@
-{ stdenv, fetchurl, buildPythonPackage, pkgconfig, glib, gobjectIntrospection, pycairo, cairo, which, ncurses}:
+{ stdenv, fetchurl, buildPythonPackage, pkgconfig, glib, gobjectIntrospection,
+pycairo, cairo, which, ncurses, meson, ninja, isPy3k }:
 
 buildPythonPackage rec {
-  major = "3.26";
-  minor = "1";
+  major = "3.30";
+  minor = "0";
   version = "${major}.${minor}";
   format = "other";
   pname = "pygobject";
@@ -10,12 +11,16 @@ buildPythonPackage rec {
 
   src = fetchurl {
     url = "mirror://gnome/sources/pygobject/${major}/${name}.tar.xz";
-    sha256 = "1afi0jdjd9sanrzjwhv7z1k7qxlb91fqa6yqc2dbpjkhkjdpnmzm";
+    sha256 = "1l3bsnpm7p5dr936ir01446p7rzpi6mp94n64r62z4nzflabl83x";
   };
 
   outputs = [ "out" "dev" ];
 
-  nativeBuildInputs = [ pkgconfig ];
+  mesonFlags = [
+    "-Dpython=python${if isPy3k then "3" else "2" }"
+  ];
+
+  nativeBuildInputs = [ pkgconfig meson ninja ];
   buildInputs = [ glib gobjectIntrospection ]
                  ++ stdenv.lib.optionals stdenv.isDarwin [ which ncurses ];
   propagatedBuildInputs = [ pycairo cairo ];
