0ddfaefa64505bd92f2624a09bdeb89de010f697
Tor Hedin Brønner <torhedinbronner@gmail.com>
2018-09-12 12:53:06 +0200

    gnome3.gnome-usage: fix build with vala_42

--- pkgs/applications/misc/gnome-usage/default.nix
+++ pkgs/applications/misc/gnome-usage/default.nix
@@ -13,6 +13,15 @@ in stdenv.mkDerivation rec {
     sha256 = "0130bwinpkz307nalw6ndi5mk38k5g6jna4gbw2916d54df6a4nq";
   };
 
+  patches = [
+    # Fails to build with vala-0.42
+    # upstream issue and fix: https://gitlab.gnome.org/GNOME/gnome-usage/issues/46
+    (fetchurl {
+      url = https://gitlab.gnome.org/GNOME/gnome-usage/uploads/fec685e39a06f1c90f207415a0964e56/0001-Fix-build-with-vala-0.42.patch;
+      sha256 = "0d7cak480xfkgndhykhjk3zlk1mk152sgg53vsm4lpvg73l8vzlz";
+    })
+  ];
+
   nativeBuildInputs = [ meson ninja pkgconfig vala gettext libxml2 desktop-file-utils wrapGAppsHook ];
 
   buildInputs = [ glib gtk3 libgtop gnome3.defaultIconTheme ];
